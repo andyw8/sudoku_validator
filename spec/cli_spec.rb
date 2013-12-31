bc794f8a2a1228b96a4f5f316e8c4a863ba48362
Andy Waite <github.aw@andywaite.com>
2013-12-31 12:55:46 +0800

    Start with a valid sudoku, and manipulate for each spec. Seems a cleaner approach.

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,33 +1,32 @@
 require_relative '../lib/cli'
 
 describe Cli do
+  # use the valid case as a 'starting point' for the stub.
+  # does this seem a reasonable approach?
+  let(:sudoku) { double('sudoku', valid?: true, incomplete?: false) }
+
   it "marks a valid sudoku as valid" do
-    valid = double('valid', valid?: true, incomplete?: false)
-    cli = Cli.new(valid)
+    cli = Cli.new(sudoku)
     expect(cli.message).to eq("This sudoku is valid.")
   end
 
   it "marks a valid but incomplete sudoku as valid but incomplete." do
-    valid_but_incomplete = double('valid_but_incomplete',
-                                  valid?: true,
-                                  incomplete?: true)
-    cli = Cli.new(valid_but_incomplete)
+    sudoku.stub(incomplete?: true)
+    cli = Cli.new(sudoku)
     expect(cli.message).to eq("This sudoku is valid, but incomplete.")
   end
 
   it "marks an invalid sudoku as invalid." do
-    invalid = double('invalid', valid?: false, incomplete?: false)
-    cli = Cli.new(invalid)
+    sudoku.stub(valid?: false)
+    cli = Cli.new(sudoku)
     expect(cli.message).to eq("This sudoku is invalid.")
   end
 
   # This spec is kinda redundant, but I'm keeping it to illustrate
   # the code performs as the requirements specify.
   it "marks an invalid incomplete sudoku as invalid." do
-    invalid_incomplete = double('invalid_incomplete',
-                                valid?: false,
-                                incomplete?: true)
-    cli = Cli.new(invalid_incomplete)
+    sudoku.stub(valid?: false, incomplete?: true)
+    cli = Cli.new(sudoku)
     expect(cli.message).to eq("This sudoku is invalid.")
   end
 end
